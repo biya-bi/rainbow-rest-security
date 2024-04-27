bae3982b9b3f644588e56600ac0996277ca3ecd4
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-04-27 12:21:14 -0400

    Initial commit

---
+++ src/main/java/org/rainbow/security/authentication/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,50 @@
+package org.rainbow.security.authentication;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.jwt.JwtValidationException;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final AuthenticationEntryPoint delegate = new BearerTokenAuthenticationEntryPoint();
+
+    private final ObjectMapper mapper;
+
+   public JwtAuthenticationEntryPoint(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+            throws IOException, ServletException {
+        delegate.commence(request, response, exception);
+        if (exception.getCause() instanceof JwtValidationException validation) {
+            response.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            
+            var detail = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
+            detail.setType(URI.create("https://tools.ietf.org/html/rfc6750#section-3.1"));
+            detail.setTitle("Invalid Token");
+            detail.setProperty("errors", validation.getErrors());
+            
+            mapper.writeValue(response.getWriter(), detail);
+        }
+    }
+
+}
+
---
+++ src/main/java/org/rainbow/security/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package org.rainbow.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationEntryPoint entryPoint)
+            throws Exception {
+        httpSecurity.authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .oauth2ResourceServer(customizer -> customizer.authenticationEntryPoint(entryPoint)
+                        .jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
+    }
+
+}
